4459d6ff4e9292a1dd3650a86f74461cd34b0ef6
Ricardo Mendes <rokusu@gmail.com>
2017-01-31 21:50:57 +0000

    yaml yaml

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import heimdall from 'heimdalljs'; // eslint-disable-line no-unused-vars
 import { default as _logger } from 'heimdalljs-logger';
 
 const logger = _logger('broccoli-yaml-to-json'); // eslint-disable-line no-unused-vars
-// const yaml = require('js-yaml');
+const yaml = require('js-yaml');
+
+function yamlDirtoJson(inputDir, outputDir) {
+  var fs = require('fs');
+  var path = require('path');
+
+  var files = fs.readdirSync(inputDir);
+
+  for (var i in files) {
+    if (path.extname(files[i]) === ".yml") {
+      var outputPath = path.join(outputDir, path.basename(files[i], '.yml'), '.json');
+      var convertedFile = yaml.safeLoad(fs.readFileSync(files[i], 'utf8'));
+
+      fs.writeFileSync(outputPath, convertedFile);
+    }
+  }
+}
 
 export default class BroccoliYamlToJson extends Plugin {
   constructor(node, options = {}) {
@@ -18,22 +34,6 @@ export default class BroccoliYamlToJson extends Plugin {
   }
 
   build() {
-    // The build logic of your plugin goes here
-    // Read files from this.inputPaths, and write files to this.outputPath.
-    // Silly example:
-
-    this.inputPaths.forEach(a => console.log("->>> " + a + "\n"));
-    // var doc = yaml.safeLoad(fs.readFileSync('/home/ixti/example.yml', 'utf8'));
-// yaml.safeLoadAll(data, function (doc) {
-// });
-
-
-    // // Read 'foo.txt' from the third input node
-    // var inputBuffer = fs.readFileSync(path.join(this.inputPaths[2], 'foo.txt'));
-    // var outputBuffer = someCompiler(inputBuffer);
-    // // Write to 'bar.txt' in this node's output
-    // fs.writeFileSync(path.join(this.outputPath, 'bar.txt'), outputBuffer);
-
-    // yaml2json(inputBuffer);
+    yamlDirtoJson(this.inputPaths[0], this.outputDir);
   }
 }
